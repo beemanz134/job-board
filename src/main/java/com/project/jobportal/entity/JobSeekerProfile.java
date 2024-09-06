8cd4cd1f987d6618149aa2c4d22bfb119e596ab4
eggsNBakey <beeman134@gmail.com>
2024-09-06 12:21:32 -0500

    update 0.2
    added filter and search functionality for jobseekers

--- src/main/java/com/project/jobportal/entity/JobSeekerProfile.java
+++ src/main/java/com/project/jobportal/entity/JobSeekerProfile.java
@@ -7,6 +7,7 @@ import java.util.List;
 @Entity
 @Table(name = "job_seeker_profile")
 public class JobSeekerProfile {
+
     @Id
     private Integer userAccountId;
 
@@ -23,11 +24,12 @@ public class JobSeekerProfile {
     private String workAuthorization;
     private String employmentType;
     private String resume;
+
     @Column(nullable = true, length = 64)
     private String profilePhoto;
+
     @OneToMany(targetEntity = Skills.class, cascade = CascadeType.ALL, mappedBy = "jobSeekerProfile")
     private List<Skills> skills;
-
 
     public JobSeekerProfile() {
     }
@@ -148,9 +150,9 @@ public class JobSeekerProfile {
     }
 
     @Transient
-    public String getPhotosImagePath(){
+    public String getPhotosImagePath() {
         if (profilePhoto == null || userAccountId == null) return null;
-        return "/photos/candidate/" + userAccountId + "/" + profilePhoto;
+        return "photos/candidate/" + userAccountId + "/" + profilePhoto;
     }
 
     @Override
--- src/main/java/com/project/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/project/jobportal/services/JobPostActivityService.java
@@ -2,22 +2,23 @@ package com.project.jobportal.services;
 
 import com.project.jobportal.entity.*;
 import com.project.jobportal.repository.JobPostActivityRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class JobPostActivityService {
-    public final JobPostActivityRepository jobPostActivityRepository;
 
-    @Autowired
+    private final JobPostActivityRepository jobPostActivityRepository;
+
     public JobPostActivityService(JobPostActivityRepository jobPostActivityRepository) {
         this.jobPostActivityRepository = jobPostActivityRepository;
     }
 
-    public JobPostActivity addNew(JobPostActivity jobPostActivity){
+    public JobPostActivity addNew(JobPostActivity jobPostActivity) {
         return jobPostActivityRepository.save(jobPostActivity);
     }
 
@@ -36,8 +37,17 @@ public class JobPostActivityService {
         return recruiterJobsDtoList;
 
     }
-    public JobPostActivity getOne(int id) {
 
+    public JobPostActivity getOne(int id) {
         return jobPostActivityRepository.findById(id).orElseThrow(()->new RuntimeException("Job not found"));
+    }
+
+    public List<JobPostActivity> getAll() {
+        return jobPostActivityRepository.findAll();
+    }
+
+    public List<JobPostActivity> search(String job, String location, List<String> type, List<String> remote, LocalDate searchDate) {
+        return Objects.isNull(searchDate) ? jobPostActivityRepository.searchWithoutDate(job, location, remote,type) :
+                jobPostActivityRepository.search(job, location, remote, type, searchDate);
     }
 }
