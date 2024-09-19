51d4fe417c72b42d19fab02b8914f9d03d6147a0
eggsNBakey <beeman134@gmail.com>
2024-09-19 16:45:13 -0500

    0.2.2
    changed db source to

--- src/test/java/com/project/jobportal/JobPortalApplicationTests.java
+++ src/test/java/com/project/jobportal/JobPortalApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JobPortalApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
